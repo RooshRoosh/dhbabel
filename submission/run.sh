abb7420f2912ab4c15398412867850671fb5e33d
RooshRoosh <roosh90@mail.ru>
2018-01-05 11:41:41 +0500

    clear comments

--- submission/run.sh
+++ submission/run.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/env bash
 
-#echo $PATH
-export PATH=/:$PATH
-#export PATH=/xbin:$PATH
-#echo $PATH
-#echo $PYTHONPATH
-
-ls -ls
-ls -lsr xbin
-
 python3 -m xbin.main infer --config config/opennmt-defaults.yml --model config/models/model.py --features_file /data/input.txt > /output/output.txt
-
-#python /xbin/main.py infer --config config/opennmt-defaults.yml --model config/models/model.py --features_file /data/input.txt > /output/output.txt
-# run -v ./input:/data -v .output:/output -t {image} {entry_point}
-
